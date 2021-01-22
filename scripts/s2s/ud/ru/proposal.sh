b57c0d654d3e1b1adc2f52012aa1ad94a248e0db
mengyuxian <mengyuxian@shannonai.com>
2021-01-22 17:42:30 +0800

    FEA/ud-ru scripts

--- scripts/s2s/ud/ru/proposal.sh
+++ scripts/s2s/ud/ru/proposal.sh
@@ -2,26 +2,27 @@
 export PYTHONPATH="$PWD"
 export TOKENIZERS_PARALLELISM=false
 
-DATA_DIR="/userhome/yuxian/data/parser/ud-treebanks/ud-treebanks-v2.2/UD_French-GSD"
-DATA_PREFIX="fr_gsd-ud-"
+DATA_DIR="/userhome/yuxian/data/parser/ud-treebanks/ud-treebanks-v2.2/UD_Russian-SynTagRus/"
 BERT_DIR="/userhome/yuxian/data/bert/xlm-roberta-large/"
 BERT_TYPE="roberta"
+DATA_PREFIX="ru_syntagrus-ud-"
 
 # hyper-params
 DROPOUT=0.3
 precision=16
-LR=2e-5
-max_epoch=10
+LR=3e-5
+max_epoch=20
 
-OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ud-fr/s2s/xlm_proposal_lr${LR}_maxepoch${max_epoch}"
+
+OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ud-ru/s2s/xlm_proposal_lr${LR}_maxepoch${max_epoch}"
+rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
 python parser/span_proposal_trainer.py \
 --bert_name $BERT_TYPE \
 --precision $precision \
---default_root_dir $OUTPUT_DIR \
+--default_root_dir $OUTPUT_DIR --data_prefix $DATA_PREFIX \
 --data_dir $DATA_DIR \
---data_prefix $DATA_PREFIX \
 --data_format 'conllu' \
 --pos_dim 100 \
 --bert_dir $BERT_DIR \
@@ -30,11 +31,12 @@ python parser/span_proposal_trainer.py \
 --workers 8 \
 --gpus="0," \
 --accelerator "ddp" \
---batch_size 16 \
---accumulate_grad_batches 8 \
+--batch_size 32 \
+--accumulate_grad_batches 4 \
 --lr $LR \
 --gradient_clip_val=1.0 \
 --ignore_punct \
 --max_epochs $max_epoch \
 --group_sample \
 --scheduler "linear_decay" --warmup_steps 100 --final_div_factor 10
+
--- scripts/s2s/ud/ru/s2s.sh
+++ scripts/s2s/ud/ru/s2s.sh
@@ -2,26 +2,27 @@
 export PYTHONPATH="$PWD"
 export TOKENIZERS_PARALLELISM=false
 
-DATA_DIR="/userhome/yuxian/data/parser/ud-treebanks/ud-treebanks-v2.2/UD_French-GSD"
-DATA_PREFIX="fr_gsd-ud-"
+DATA_DIR="/userhome/yuxian/data/parser/ud-treebanks/ud-treebanks-v2.2/UD_Russian-SynTagRus/"
 BERT_DIR="/userhome/yuxian/data/bert/xlm-roberta-large/"
 BERT_TYPE="roberta"
+DATA_PREFIX="ru_syntagrus-ud-"
+
 
 # hyper-params
 DROPOUT=0.3
-accumulate=150
-WARMUP=100
+accumulate=80
+WARMUP=300
 addition=1
-LR=4e-5
+LR=2e-5
 
 # save directory
-OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ud-fr/s2s/lr${LR}_accumulate${accumulate}_warmup${WARMUP}_add${addition}"
+OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ud-ru/s2s/lr${LR}_accumulate${accumulate}_warmup${WARMUP}_add${addition}"
 mkdir -p $OUTPUT_DIR
 
 python parser/s2s_query_trainer.py \
 --precision 16 \
---default_root_dir $OUTPUT_DIR \
---data_dir $DATA_DIR --data_prefix $DATA_PREFIX \
+--default_root_dir $OUTPUT_DIR --data_prefix $DATA_PREFIX \
+--data_dir $DATA_DIR \
 --data_format 'conllu' \
 --pos_dim 100 \
 --bert_dir $BERT_DIR --bert_name $BERT_TYPE \
@@ -31,7 +32,7 @@ python parser/s2s_query_trainer.py \
 --workers 24 \
 --gpus="0,1,2,3" \
 --accelerator "ddp" \
---batch_size 4 \
+--batch_size 8 \
 --accumulate_grad_batches $accumulate \
 --lr $LR \
 --gradient_clip_val=1.0 \
@@ -39,4 +40,4 @@ python parser/s2s_query_trainer.py \
 --max_epochs 10 \
 --group_sample \
 --scheduler "linear_decay" --warmup_steps $WARMUP --final_div_factor 20
-d
+
