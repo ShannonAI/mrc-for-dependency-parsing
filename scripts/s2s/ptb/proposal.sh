f488c8c6e5dd24554c2c0d045ad4d9258471a586
mengyuxian <mengyuxian@shannonai.com>
2021-02-02 10:47:09 +0800

    FEA/new scripts

--- scripts/s2s/ptb/proposal.sh
+++ scripts/s2s/ptb/proposal.sh
@@ -1,3 +1,4 @@
+cd /userhome/yuxian/shannon_parser
 export PYTHONPATH="$PWD"
 export TOKENIZERS_PARALLELISM=false
 
@@ -6,10 +7,11 @@ BERT_DIR="/userhome/yuxian/data/bert/roberta-large"
 BERT_TYPE="roberta"
 # hyper-params
 DROPOUT=0.3
-LR=1e-5
+LR=2e-5
 precision=16
+add=2
 
-OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ptb/s2s/roberta_proposal_lr${LR}"
+OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ptb/s2s/new_data_roberta_proposal_lr${LR}_add${add}"
 mkdir -p $OUTPUT_DIR
 
 python parser/span_proposal_trainer.py \
@@ -20,16 +22,17 @@ python parser/span_proposal_trainer.py \
 --data_format 'conllx' \
 --pos_dim 100 \
 --bert_dir $BERT_DIR \
---additional_layer_dim 1124 \
+--additional_layer_dim 1024 \
+--additional_layer $add --additional_layer_type "transformer" \
 --mrc_dropout $DROPOUT \
 --workers 8 \
---gpus="0,1" \
+--gpus="2,3" \
 --accelerator "ddp" \
 --batch_size 64 \
 --accumulate_grad_batches 1 \
 --lr $LR \
 --gradient_clip_val=1.0 \
 --ignore_punct \
---max_epochs 10 \
+--max_epochs 30 \
 --group_sample \
 --scheduler "linear_decay" --warmup_steps 100 --final_div_factor 10
--- scripts/s2s/ptb/s2s.sh
+++ scripts/s2s/ptb/s2s.sh
@@ -1,20 +1,24 @@
+cd /userhome/yuxian/shannon_parser
 export PYTHONPATH="$PWD"
 export TOKENIZERS_PARALLELISM=false
 
 DATA_DIR="/userhome/yuxian/data/parser/ptb3_parser"
-#BERT_DIR="/userhome/yuxian/data/bert/bert-large-uncased-wwm"
 BERT_DIR="/userhome/yuxian/data/bert/roberta-large"
 BERT_TYPE="roberta"
+
 # hyper-params
 DROPOUT=0.3
-LR=1e-5
-bert_dropout=0.1
+LR=2e-5
 DECAY=0.0
 accumulate=20
 WARMUP=300
-addition=1
+add=1
+norm="sigmoid"
+tag_weight=2.0
+layer_type="lstm"
 
-OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ptb/s2s/roberta_new_bertdrop${bert_dropout}_decay${DECAY}_accumulate${accumulate}_additional_layer${addition}"
+# save directory
+OUTPUT_DIR="/userhome/yuxian/train_logs/dependency/ptb-s2s-new/s2s_lr${LR}_decay${DECAY}_accumulate${accumulate}_warmup${WARMUP}_add${add}_norm${norm}_tweight${tag_weight}_${layer_type}"
 mkdir -p $OUTPUT_DIR
 
 python parser/s2s_query_trainer.py \
@@ -22,10 +26,10 @@ python parser/s2s_query_trainer.py \
 --default_root_dir $OUTPUT_DIR \
 --data_dir $DATA_DIR \
 --data_format 'conllx' \
---pos_dim 100 \
+--pos_dim 100 --normalize $norm --tag_loss_weight $tag_weight \
 --bert_dir $BERT_DIR --bert_name $BERT_TYPE \
 --additional_layer_dim 1024 \
---additional_layer 1 --additional_layer_type "transformer" \
+--additional_layer $add --additional_layer_type $layer_type \
 --mrc_dropout $DROPOUT \
 --workers 24 \
 --gpus="0,1,2,3,4,5,6,7" \
@@ -34,11 +38,8 @@ python parser/s2s_query_trainer.py \
 --accumulate_grad_batches $accumulate \
 --lr $LR \
 --gradient_clip_val=1.0 \
---ignore_punct \
---max_epochs 10 \
+--ignore_punct --predict_child \
+--max_epochs 20 \
 --group_sample \
---bert_dropout $bert_dropout --weight_decay $DECAY \
+--weight_decay $DECAY \
 --scheduler "linear_decay" --warmup_steps $WARMUP --final_div_factor 20
-
-
-# todo 用小batch和小lr，random搞起来，一个epoch evaluate多次
